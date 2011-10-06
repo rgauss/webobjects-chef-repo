24c4ebbd7fa8ee6d4a9d686ebe6d2f4733a2be09
Ray Gauss II <rgauss@rgauss.com>
2011-10-06 18:25:40 -0400

    Fixed cookbook names

--- roles/webobjects_application_server.rb
+++ roles/webobjects_application_server.rb
@@ -1,4 +1,4 @@
 
 name "webobjects_application_server"
 description "A WebObjects application server"
-run_list "recipe[java]", "recipe[webobjects_wonder::modify_system_file_limits]", "recipe[webobjects_wonder::setup_deployment_environment]"
+run_list "recipe[java]", "recipe[webobjects::modify_system_file_limits]", "recipe[webobjects::setup_deployment_environment]"
--- roles/webobjects_http_server.rb
+++ roles/webobjects_http_server.rb
@@ -1,4 +1,4 @@
 
 name "webobjects_http_server"
 description "A server which handles HTTP requests and pass them via wonder adapter to a WebObjects application server"
-run_list "recipe[apache2]", "recipe[apache2::mod_ssl]", "recipe[webobjects_wonder::build_apache_adaptor]", "recipe[webobjects_wonder::modify_apache_config]"
+run_list "recipe[apache2]", "recipe[apache2::mod_ssl]", "recipe[webobjects::build_apache_adaptor]", "recipe[webobjects::modify_apache_config]"
