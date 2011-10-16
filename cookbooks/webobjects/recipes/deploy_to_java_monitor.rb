2917ceda8fc1d56b5cef25c4298020b255c240d6
Ray Gauss II <rgauss@rgauss.com>
2011-10-16 11:41:26 -0400

    Added ability to specify multiple instances.

--- cookbooks/webobjects/recipes/deploy_to_java_monitor.rb
+++ cookbooks/webobjects/recipes/deploy_to_java_monitor.rb
@@ -13,12 +13,24 @@ additional_arguments_string = ""
 if !app['webobjects_additional_arguments'].nil?
   additional_arguments_string = ", additionalArgs: '#{app['webobjects_additional_arguments']}'"
 end
+num_instances = 1
+if !app['webobjects_num_instances'].nil?
+  num_instances = app['webobjects_num_instances']
+end
   
 script "deploy_to_java_monitor" do
   interpreter "bash"
   code <<-EOH
   curl -X DELETE http://#{node[:webobjects][:webobjects_JavaMonitor_host]}:#{node[:webobjects][:webobjects_JavaMonitor_port]}/#{node[:webobjects][:webobjects_apps_url]}/JavaMonitor.woa/ra/mApplications/#{app['deploy_name']}?pw=#{node[:webobjects][:webobjects_JavaMonitor_password]}
   curl -X POST -d "{id: '#{app['deploy_name']}',type: 'MApplication', name: '#{app['deploy_name']}', unixOutputPath: '#{node[:webobjects][:webobjects_WOLog_dir]}', unixPath: '#{node[:webobjects][:webobjects_WOApplications_dir]}/#{app['deploy_name']}.woa/#{app['deploy_name']}'#{receive_timeout_string}#{connect_timeout_string}#{additional_arguments_string}}" http://#{node[:webobjects][:webobjects_JavaMonitor_host]}:#{node[:webobjects][:webobjects_JavaMonitor_port]}/#{node[:webobjects][:webobjects_apps_url]}/JavaMonitor.woa/ra/mApplications.json?pw=#{node[:webobjects][:webobjects_JavaMonitor_password]}
-  curl -X GET http://#{node[:webobjects][:webobjects_JavaMonitor_host]}:#{node[:webobjects][:webobjects_JavaMonitor_port]}/#{node[:webobjects][:webobjects_apps_url]}/JavaMonitor.woa/ra/mApplications/#{app['deploy_name']}/addInstance?pw=#{node[:webobjects][:webobjects_JavaMonitor_password]}
   EOH
+end
+
+for i in 1..num_instances
+  script "add_instance_to_java_monitor" do
+    interpreter "bash"
+    code <<-EOH
+    curl -X GET http://#{node[:webobjects][:webobjects_JavaMonitor_host]}:#{node[:webobjects][:webobjects_JavaMonitor_port]}/#{node[:webobjects][:webobjects_apps_url]}/JavaMonitor.woa/ra/mApplications/#{app['deploy_name']}/addInstance?pw=#{node[:webobjects][:webobjects_JavaMonitor_password]}
+    EOH
+  end
 end
