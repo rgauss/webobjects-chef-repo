d0aa17a91eccd61ddcd40d510955e54962fa388d
Ray Gauss II <rgauss2@gmail.com>
2013-04-12 14:59:20 -0400

    Removal of files in prep for deletion of repo in favor of https://github.com/rgauss/chef-webobjects

--- roles/webobjects_application_server.rb
+++
@@ -1,4 +0,0 @@
-
-name "webobjects_application_server"
-description "A WebObjects application server"
-run_list "recipe[java]", "recipe[webobjects::modify_system_file_limits]", "recipe[webobjects::setup_deployment_environment]"
--- roles/webobjects_compile_server.rb
+++
@@ -1,4 +0,0 @@
-
-name "webobjects_compile_server"
-description "A server which compiles WebObjects apps"
-run_list "role[webobjects_application_server]", "recipe[webobjects::setup_compile_environment]"
